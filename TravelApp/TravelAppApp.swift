4cd1abbdae62099c31064e056108d85b776cb663
Tiara <tiaranurulh@gmail.com>
2022-11-26 18:36:13 +0700

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Tiara H on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
